869343e05e8e989ea6643257495a67a1f3b06b64
Frank Shaw <fhshaw7@gmail.com>
2015-02-23 01:40:25 -0800

    search leads to images appearing

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -1,7 +1,31 @@
-(function(){
+// (function(){
+function initialize() {
+  var lat = 37.765
+  var lng = -122.4167
+  var mapOptions = {
+    center: { lat: lat, lng: lng},
+    zoom: 12
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var input = (document.getElementById('pac-input'));
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+    lat = places[0]["geometry"]["location"]["k"]
+    lng = places[0]["geometry"]["location"]["D"]
+    return grabInstaPics(lat,lng,map)
+  })
+};
+
+function grabInstaPics(lat, lng, map){
   var client_id = "44bc6fcf9a344b4ba7f7a5abc8540ba6"
-  var lat = "37.819929"
-  var lng = "-122.478255"
+  var lat = lat
+  var lng = lng
   var media_data = []
   var minDelayMins = 15
   var MS_PER_MIN = 60000
@@ -12,48 +36,40 @@
     dataType: "jsonp",
     type: "GET",
   }).done(function(data){
+    console.log(data)
+
     mediaResponseData = data
-    initialize(mediaResponseData)
     var data_length = data.data.length
     for (var i = 0; i < data_length; i++) {
       $("#pics").append("<a href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
     }
+    displayInstaPics(mediaResponseData,map)
   }).fail(function(){
     console.log("Failed at media location GET request")
   })
+};
 
-
-  function initialize(mediaResponseData) {
-    var mapOptions = {
-      center: { lat: 37.765, lng: -122.4733},
-      zoom: 14
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-
-for (var i = 0; i< mediaResponseData["data"].length; i++){
-  var instaLat = mediaResponseData["data"][i]["location"]["latitude"]
-  var instaLng = mediaResponseData["data"][i]["location"]["longitude"]
-  var instaImg = mediaResponseData["data"][i]["images"]["low_resolution"]["url"]
-  var formatedInstaData = latLngImg(instaLat,instaLng,instaImg)
-  console.log(formatedInstaData[0])
-  var marker = new google.maps.Marker({
-    position: formatedInstaData[0],
-    map: map,
-    icon: new google.maps.MarkerImage(
-      formatedInstaData[2],
-      null,
-      null,
-      null,
-      new google.maps.Size(80, 80)
-      )
-
-  })
-}
-function latLngImg(instaLat,instaLng,instaImg){
-  return ([{ lat: instaLat, lng: instaLng}, map, instaImg])
-}
-}
-
-})();
+function displayInstaPics(mediaResponseData,map){
+  console.log(mediaResponseData)
+  for (var i = 0; i< mediaResponseData["data"].length; i++){
+    var instaLat = mediaResponseData["data"][i]["location"]["latitude"]
+    var instaLng = mediaResponseData["data"][i]["location"]["longitude"]
+    var instaImg = mediaResponseData["data"][i]["images"]["low_resolution"]["url"]
+    var formatedInstaData = latLngImg(instaLat,instaLng,instaImg)
+    var marker = new google.maps.Marker({
+      position: formatedInstaData[0],
+      map: map,
+      icon: new google.maps.MarkerImage(
+        formatedInstaData[2],
+        null,
+        null,
+        null,
+        new google.maps.Size(80, 80)
+        )
+    })
+  };
+  function latLngImg(instaLat,instaLng,instaImg){
+    return ([{ lat: instaLat, lng: instaLng}, map, instaImg])
+  }
+};
+// })();
